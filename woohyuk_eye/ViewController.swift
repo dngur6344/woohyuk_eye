a612a6c399b66f635e623d90a5219253d603dd7f
dngur6344 <dngur6344@gmail.com>
2019-04-01 14:30:46 +0900

    ver2

--- woohyuk_eye/ViewController.swift
+++ woohyuk_eye/ViewController.swift
@@ -13,13 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
     }
+    @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
+    var currentVal=50
+    var targetValue=0
+    var score=0
     override func didReceiveMemoryWarning(){
         super.didReceiveMemoryWarning()
     }
     @IBAction func showAlert(){
-        let message = "The value of the slider is: \(currentValue)"
+        let difference =  abs(targetValue-currentVal)
+        let points = 100 - difference
+        
+        let message = "You scored \(points)"
         let alert = UIAlertController(title: "Hello, World",message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome",style: .default, handler: nil)
         alert.addAction(action)
@@ -28,6 +36,15 @@ class ViewController: UIViewController {
     @IBAction func sliderMoved(_ slider: UISlider){
         currentValue = lroundf(slider.value)
         print(" The value of the slider is now: \(currentValue)")
+    }
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue=50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    func updateLabels(){
+        targetLabel.text=String(targetValue)
     }
 }
 
