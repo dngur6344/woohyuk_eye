836be314b8266dbdf5a8fc5129473a376fd1823d
dngur6344 <dngur6344@gmail.com>
2019-04-01 21:48:18 +0900

    ver7

--- woohyuk_eye/AboutViewController.swift
+++ woohyuk_eye/AboutViewController.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
-
+import WebKit
 class AboutViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: WKWebView!
+    
     @IBAction func close(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let url = Bundle.main.url(forResource: "Bulls",withExtension:"html"){
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
         // Do any additional setup after loading the view.
     }
     
