33ddc16dc7a4fbddbb19654d9731c77d02658a3c
dngur6344 <dngur6344@gmail.com>
2019-04-01 21:02:37 +0900

    ver6

---
+++ woohyuk_eye/AboutViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AboutViewController.swift
+//  woohyuk_eye
+//
+//  Created by user on 01/04/2019.
+//  Copyright © 2019 woohyuk. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
